845470004fd8ac84637a11a7dd25827109600c03
alan <alan@wireless-tag.com>
2021-04-29 10:22:18 +0800

    procd: fix flock dead lock

--- 18.06/package/system/procd/files/procd.sh
+++ 18.06/package/system/procd/files/procd.sh
@@ -44,6 +44,7 @@ procd_lock() {
 
 	flock -n 1000 &> /dev/null
 	if [ "$?" != "0" ]; then
+		rm -rf "$IPKG_INSTROOT/var/lock/procd_${service_name}.lock"
 		exec 1000>"$IPKG_INSTROOT/var/lock/procd_${service_name}.lock"
 		flock 1000
 		if [ "$?" != "0" ]; then
